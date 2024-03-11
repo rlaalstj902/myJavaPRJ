7128b475ccb5197e14b7262cb6c447209bc52d09
rlaalstj902 <rlaalstj295@gmail.com>
2024-03-11 15:08:10 +0900

    소스수정

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,8 @@
 public class Hello {
 
- public static void main(String[] args){
-    System.out.println("Hello!! My First Java Coding!!");
- }
+    public static void main(String[] args) {
+        System.out.println("Hello!! My First Java Coding!!");
+
+        System.out.println("Edit Java Code!!");
+    }
 }
