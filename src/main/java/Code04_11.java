74460d7c9f24eb098d628e260e1212a8a9df3d95
rlaalstj902 <rlaalstj295@gmail.com>
2024-03-25 14:00:59 +0900

    Code04_11

--- src/main/java/Code04_11.java
+++ src/main/java/Code04_11.java
@@ -5,9 +5,9 @@ public class Code04_11 {
         String var1,var2;
         int diff;
 
-        System.out.println("첫 번째 문자열 ==> ");
+        System.out.print("첫 번째 문자열 ==> ");
         var1 = s.nextLine();
-        System.out.println("두 번째 문자열 ==> ");
+        System.out.print("두 번째 문자열 ==> ");
         var2 = s.nextLine();
 
         diff = var1.length() - var2.length();
