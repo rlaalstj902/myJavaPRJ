05a5071e896d5c0a30acb8b4c4f3c6ce6a3428ac
rlaalstj902 <rlaalstj295@gmail.com>
2024-03-25 13:34:28 +0900

    Code04_09

---
+++ src/main/java/Code02_09_1.java
@@ -0,0 +1,18 @@
+public class Code02_09_1 {
+    public static void main(String[] args) {
+        int number1 = 10, number2 = 20;
+        int resAdd;
+        int resSub;
+        int resMul;
+        int resDiv;
+
+        resAdd = number1 + number2;
+        resSub = number1 - number2;
+        resMul = number1 * number2;
+        resDiv = number1 / number2;
+
+        System.out.println(number1 + " + " + number2 + " = " + resAdd);
+        System.out.println(number1 + " - " + number2 + " = " + resSub);
+        System.out.println(number1 );
+    }
+}
