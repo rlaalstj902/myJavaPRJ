8904dadb4f109349ae9559f92a1a9b678b417f9a
rlaalstj902 <rlaalstj295@gmail.com>
2024-03-25 14:27:17 +0900

    Code04_15

--- src/main/java/Code04_15.java
+++ src/main/java/Code04_15.java
@@ -3,7 +3,7 @@ public class Code04_15 {
         String str = "난생처음 자바를 처음 학습 중입니다. 자바는 처음이지만 재미있네요.";
 
         System.out.println(str.indexOf("처음"));
-        System.out.println(str.indexOf("처음"));
+        System.out.println(str.lastIndexOf("처음"));
         System.out.println(str.indexOf("처음",4));
     }
 }
