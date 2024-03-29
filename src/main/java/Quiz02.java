eb42bdefdf2caa4e5aa75bdb8ed046dff8478da9
rlaalstj902 <rlaalstj295@gmail.com>
2024-03-29 12:02:58 +0900

    삼각형 그리기2

---
+++ src/main/java/Quiz02.java
@@ -0,0 +1,17 @@
+public class Quiz02 {
+    public static void main(String[] args) {
+        for (int i = 1 ; i <=7 ; i=i+2){
+            for (int j =1 ; j<=7 ; j++){
+                if (j<=(7-i)/2){
+                    System.out.print(" ");
+                } else if (j > 7-(7-i)/2) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+
+        }
+    }
+}
