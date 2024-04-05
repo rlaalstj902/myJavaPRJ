a21eeac0eb54bdf75ee6229d880980ab4f9a41f5
rlaalstj902 <rlaalstj295@gmail.com>
2024-04-05 13:44:10 +0900

    Quiz03

--- src/main/java/Quiz03.java
+++ src/main/java/Quiz03.java
@@ -4,19 +4,19 @@ public class Quiz03 {
         Scanner s = new Scanner(System.in);
         int num;
 
-        System.out.print("숫자 입력 ==> ");
+        System.out.print("숫자 입력 : ");
         num = s.nextInt();
 
-        if (num %21 ==0 ) {
-            System.out.println("3과 7의 배수 모두 만족합니다. ");
+        if (num%21 == 0) {
+            System.out.println("3과 7의 배수입니다. ");
 
-        } else if (num%3 == 0) {
-            System.out.println("3의 배수입니다.");
+        }else if (num%3 == 0) {
+            System.out.println("3의 배수입니다. ");
 
-        } else if (num%7 == 0) {
-            System.out.println("7의 배수입니다.");
-        }
-        else{
+        } else if(num%7 ==0 ) {
+            System.out.println("7의 배수입니다. ");
+
+        }else {
             System.out.println("3과 7의 배수 모두 만족하지 않습니다. ");
         }
     }
