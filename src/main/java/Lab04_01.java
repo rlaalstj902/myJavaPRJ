04d531984d4ed19f49b2a8db2a8af066a7d40b78
rlaalstj902 <rlaalstj295@gmail.com>
2024-04-06 15:57:07 +0900

    Code05_04

---
+++ src/main/java/Lab04_01.java
@@ -0,0 +1,11 @@
+public class Lab04_01 {
+    public static void main(String[] args) {
+        String ss ="세븐틴";
+        System.out.println("원본 문자열 ==> " + ss);
+
+        System.out.print("반대 문자열 ==> ");
+        System.out.print(ss.charAt(2));
+        System.out.print(ss.charAt(1));
+        System.out.print(ss.charAt(0));
+    }
+}
