70f607455e66d73ad4324c3bac7111854d12e48d
rlaalstj902 <rlaalstj295@gmail.com>
2024-04-09 15:49:42 +0900

    Quiz0409

---
+++ src/main/java/Quiz0409.java
@@ -0,0 +1,9 @@
+public class Quiz0409 {
+    public static void main(String[] args) {
+        int a,b;
+
+        for (int i=100; i<=200; i++) {
+            System.out.println(i);
+        }
+    }
+}
