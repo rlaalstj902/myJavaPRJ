2697d60245e6fe13e23f4e8d858a5472a2616ace
rlaalstj902 <rlaalstj295@gmail.com>
2024-04-17 11:23:03 +0900

    Quiz0415

---
+++ src/main/java/Quiz0415.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Quiz0415 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int hap = 0;
+
+        System.out.print("첫 번째 : ");
+        int a =s.nextInt();
+        System.out.print("두 번째: ");
+        int b = s.nextInt();
+
+        if (a < b) {
+            for (int i =a ; i <= b; i++) {
+                if (i%2 !=0 ) {
+                    hap += i;
+                }
+            }
+        }
+        else if (a > b) {
+            for (int i =a ; i >= b; i--) {
+                if (i%2 !=0) {
+                    hap += i;
+                }
+            }
+        }
+        System.out.println(a + "부터" + b + "까지 홀수의 합 :" +hap);
+    }
+}
