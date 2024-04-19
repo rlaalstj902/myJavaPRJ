cfe91209a5e4af5f6ad5fecb4d7733c8619b42ee
rlaalstj902 <rlaalstj295@gmail.com>
2024-04-19 13:08:32 +0900

    Examm

---
+++ src/main/java/Examm.java
@@ -0,0 +1,13 @@
+public class Examm {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 4; i++) {
+            for(int k = 1; k < 4-i; k++) {
+                System.out.print(" ");
+            } for (int j =0; j <i*2+1; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
