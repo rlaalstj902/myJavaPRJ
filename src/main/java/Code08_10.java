6a4ad2e3901b2c816c70391c63c4cb9357a20b8e
rlaalstj902 <rlaalstj295@gmail.com>
2024-05-21 09:19:01 +0900

    Code08_10

--- src/main/java/Code08_10.java
+++ src/main/java/Code08_10.java
@@ -19,7 +19,7 @@ public class Code08_10 {
     public static void main(String[] args) {
         Tank tank1 = new Tank();
         tank1.move();
-        tank1. fire();
+        tank1.fire();
 
     }
 }
