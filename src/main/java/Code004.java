71b2b273251082ae6026e598eb9fd75ce4b99e25
rlaalstj902 <rlaalstj295@gmail.com>
2024-12-19 23:44:47 +0900

    Code004.java

---
+++ src/main/java/Code004.java
@@ -0,0 +1,13 @@
+public class Code004 {
+
+    public static void main(String[] args) {
+        int i;
+        for (int s= 13; s>0; s--) {
+            for (i=0; i<s; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
