7ad0e9fc47c52357881fa58e2d50a95da75243b0
rlaalstj902 <rlaalstj295@gmail.com>
2024-12-25 22:40:58 +0900

    Exam05.java

---
+++ src/main/java/Exxam05.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Exxam05 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("첫 번째: ");
+        int a = s.nextInt();
+        System.out.print("두 번째: ");
+        int b = s.nextInt();
+
+        if (a < b) {
+            for (int i = a; i < b; i++) {
+                if (i % 3 == 0) {
+                    System.out.println(i);
+                }
+            }
+        } else if (a > b) {
+            for (int i = a; i > b; i--) {
+                if (i % 3 == 0) {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+}
