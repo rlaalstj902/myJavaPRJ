0e9fabcdb2d8576e6e4f1a8ef2c182db9e90b64b
rlaalstj902 <rlaalstj295@gmail.com>
2024-12-27 14:23:47 +0900

    241227.java

---
+++ src/main/java/Code241227.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Code241227 {
+        public static void main(String[] args) {
+
+            String message = "Let's go!";
+
+            System.out.println("3 \n2 \n1");
+            System.out.println(message);
+        }
+    }
