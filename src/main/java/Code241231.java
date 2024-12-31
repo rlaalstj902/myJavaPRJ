9a469b171317313a955127f73d85eeece48e4b5b
rlaalstj902 <rlaalstj295@gmail.com>
2024-12-31 14:44:07 +0900

    241231.java

---
+++ src/main/java/Code241231.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Code241231 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int answer = 0;
+
+        for (; number > 0; ) { // 조건을 수정하여 number가 0보다 클 동안 반복
+            answer += number % 100;
+            number /= 100;
+        }
+
+        System.out.println(answer);
+    }
+}
+
